428d44e4ca78b7f8359e3ea28392817d00c6adf9
jyh7a <jyh7a@naver.com>
2020-07-17 03:15:22 +0900

    비밀 정보 보호

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const config = require('./config/key')
+const { User } = require('./models/User')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+// application/json
+app.use(bodyParser.json())
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jyh7a:iEHH8VtNs8TYVaA@boilerlpate.5xj6r.mongodb.net/<dbname>?retryWrites=true&w=majority',
+mongoose.connect(config.mongoURI,
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -12,6 +22,24 @@ mongoose.connect('mongodb+srv://jyh7a:iEHH8VtNs8TYVaA@boilerlpate.5xj6r.mongodb.
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', (req, res) => res.send('Hello World! 새해복 많이받으세요'))
+
+
+// 회원가입을 위한 Router
+app.post('/register', (req, res) => {
+  // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body)
+  // save is mongoDB method 
+  user.save((err, userInfo) => {
+    if(err) return res.json({sucess: false, err})
+    return res.status(200).json({
+      sucess: true
+    })
+  })
+})
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
