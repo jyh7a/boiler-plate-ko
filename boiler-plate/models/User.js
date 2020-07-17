5263206b7e6158ffd6c65ee0ae95371f33c637ac
jyh7a <jyh7a@naver.com>
2020-07-18 01:44:47 +0900

    node js complete

--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const saltRounds = 10;
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
   name: {
@@ -46,8 +47,48 @@ userSchema.pre('save', function (next) {
         next()
       })
     })
+  }else{
+    next()
   }
 })
+
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+  // plainPassword 1234567 암호화된 비밀번호 kebyXLUHub07B7AE1YxOyDc8bBatYgtx4H0X5jkbq0D2dsOKUEW
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+    if(err) return cb(err)
+    return cb(null, isMatch)
+  })
+}
+
+
+userSchema.methods.generateToken = function(cb){
+  var user = this
+
+  // jsonwebtoken 을 이용해서 토큰생성
+  var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+  user.token = token
+  user.save(function(err, user){
+    if(err) return cb(err)
+    return cb(null, user)
+  })
+}
+
+
+userSchema.statics.findByToken = function(token, cb){
+  var user = this
+
+  // 토큰을 decode 한다
+  jwt.verify(token, 'secretToken', function(err, decoded){
+    // 유저 아이디를 이용해서 유저를 찾고
+    // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+    user.findOne({'_id': decoded, 'token': token}, function(err, user){
+      if(err) return cb(err)
+      cb(null, user)
+    })
+  })
+}
 
 const User = mongoose.model('User', userSchema)
 
