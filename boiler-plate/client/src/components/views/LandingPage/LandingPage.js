e421689cbeafe33cc12f7a8e236b9f4e4a0db1c2
jyh7a <jyh7a@naver.com>
2020-07-26 03:38:53 +0900

    login Complete

--- boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import axios from 'axios'
 
 const LandingPage = () => {
+  const centerDiv = {display:'flex', justifyContent:'center', alignItems: 'center', width:'100%', height:'100vh'}
 
   useEffect(() => {
     axios.get('/api/hello')
@@ -9,8 +10,8 @@ const LandingPage = () => {
   }, [])
 
   return (
-    <div>
-      LandingPage
+    <div style={centerDiv}>
+      <h1>시작페이지</h1>
     </div>
   );
 };
