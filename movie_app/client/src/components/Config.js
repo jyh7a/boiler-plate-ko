57cd9579a86dcfd6db87b0567314ec7cbb4632f6
jyh7a <jyh7a@naver.com>
2020-07-27 07:25:58 +0900

    main component

--- movie_app/client/src/components/Config.js
+++ movie_app/client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/'
+export const API_KEY = '39776c4db4f591864b15de5b257e8787'
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/'
